a0617375badc68ae5e4ff30f8df34aa525752f6d
arjunpraba <amuthapraba165@gmail.com>
2025-01-04 18:07:46 +0530

    new commit

---
+++ wocus/src/test/java/com/ijse/jwt/WocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
