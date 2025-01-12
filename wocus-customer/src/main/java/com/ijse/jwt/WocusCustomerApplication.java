9e589e967fee66173d5ebade9a60337abe1e408e
arjunpraba <amuthapraba165@gmail.com>
2025-01-12 07:37:08 +0530

    customer commit

---
+++ wocus-customer/src/main/java/com/ijse/jwt/WocusCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.ijse.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WocusCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WocusCustomerApplication.class, args);
+	}
+
+}
